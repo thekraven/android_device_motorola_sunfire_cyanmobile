53623aac262791fd15957572ba62ba7eb45b5e37
thekraven <savethestyle1@gmail.com>
2012-09-30 21:07:23 -0500

    try bt fix1
    Change-Id: I5f56b2e8512bea22d47d9310be60880f76777509

--- sunfire.mk
+++ sunfire.mk
@@ -17,7 +17,7 @@ $(call inherit-product-if-exists, vendor/moto/sunfire/sunfire-vendor.mk)
 PRODUCT_COPY_FILES += \
     device/motorola/sunfire/scripts/pds_perm_fix.sh:system/bin/pds_perm_fix.sh \
     device/motorola/sunfire/scripts/bt_init_wrapper.sh:system/bin/bt_init_wrapper.sh \
-    device/motorola/sunfire/scripts/hciattach_wrapper.sh:system/bin/hciattach_wrapper.sh
+#device/motorola/sunfire/scripts/hciattach_wrapper.sh:system/bin/hciattach_wrapper.sh
 
 # sysctl conf
 PRODUCT_COPY_FILES += \
